f872e681eb4c8d310b329f84b7358ffb71483b91
Fabian Mendez <ofmendez@gmail.com>
2020-06-12 11:08:37 -0500

    Agregado icono, remaquetadas pantallas registro/login

--- Assets/APP/Login.cs
+++ Assets/APP/Login.cs
@@ -1,14 +1,43 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class Login : MonoBehaviour {
-    
-    void Start() {
+    public Button submit;
+    public Button goToRegister;
+    public InputField userIf;
+    public InputField passwordIf;
+    Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
 
+    void Start() {
+        submit.onClick.AddListener(()=> {
+            ValidateFields(userIf.text,passwordIf.text);
+		});
+    }
+    public void ValidateFields(string user, string password) {
+        // if
     }
 
-    // Update is called once per frame
+
+    public void Signing(string email, string password) {
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+            if (task.IsCanceled) {
+                Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted) {
+                Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
     void Update() {
 
     }
