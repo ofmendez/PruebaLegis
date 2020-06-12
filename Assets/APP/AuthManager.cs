c0ca5fd9b70bb2787242cc83cf1bda3505c067e3
Fabian Mendez <ofmendez@gmail.com>
2020-06-12 09:29:04 -0500

    first

---
+++ Assets/APP/AuthManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Firebase;
+using Firebase.Auth;
+using Firebase.Unity.Editor;
+using UnityEngine;
+
+public class AuthManager : MonoBehaviour {
+    // Start is called before the first frame update
+    Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+    void Start() {
+    }
+
+    public void SignOut() {
+        auth.SignOut();
+    }
+    public void Signing(string email, string password) {
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+            if (task.IsCanceled) {
+                Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted) {
+                Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+    public void CreateAccount(string email, string password) {
+
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+            if (task.IsCanceled) {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted) {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+            // Firebase user has been created.
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+    // Update is called once per frame
+}
---
+++ Assets/APP/DbManager.cs
@@ -0,0 +1,48 @@
+﻿using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DbManager : MonoBehaviour {
+    DatabaseReference reference;
+    // Start is called before the first frame update
+    void Start() {
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://pruebalegis.firebaseio.com/");
+        // Get the root reference location of the database.
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+        Initialize();
+        WriteNewUser("0001", "fabian", "fabian@hotmail.com");
+    }
+
+    public async void Initialize() {
+        var result = await FirebaseApp.CheckAndFixDependenciesAsync();
+        if (result == DependencyStatus.Available) {
+
+        } else {
+            Debug.LogError("ALV");
+        }
+    }
+    // Update is called once per frame
+    public void WriteNewUser(string userId, string name, string email) {
+        User user = new User(name, email);
+        string json = JsonUtility.ToJson(user);
+
+        reference.Child("users").Child(userId).SetRawJsonValueAsync(json);
+        //SceneManager.LoadScene("ChooseLogged");
+    }
+}
+public class User {
+    public string username;
+    public string email;
+
+    public User() {
+    }
+
+    public User(string username, string email) {
+        this.username = username;
+        this.email = email;
+    }
+}
